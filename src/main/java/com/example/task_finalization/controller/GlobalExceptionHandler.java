d4ee97ad343a649f2d9281ce64dd38d9a57598b9
mihovilcrnkovic <mihovil.crnkovic@sigit.hr>
2026-01-29 11:20:46 +0100

    added GlobalExceptionHandler

---
+++ src/main/java/com/example/task_finalization/controller/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.task_finalization.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity handleException(Exception e) {
+        return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
