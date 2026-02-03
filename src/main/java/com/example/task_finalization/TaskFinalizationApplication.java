570844054e1deb34438db08e07b48e98e6e329fb
mihovilcrnkovic <mihovil.crnkovic@sigit.hr>
2026-02-03 14:00:42 +0100

    changed kafka dependency, removed KafkaConfig (auto configuration from application.properties)

--- src/main/java/com/example/task_finalization/TaskFinalizationApplication.java
+++ src/main/java/com/example/task_finalization/TaskFinalizationApplication.java
@@ -2,7 +2,9 @@ package com.example.task_finalization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class TaskFinalizationApplication {
 
