ab28068a0dc173a2079d455a555e59b930d70974
mihovilcrnkovic <mihovil.crnkovic@sigit.hr>
2026-02-05 16:16:47 +0100

    added verifyToken method

--- src/main/java/com/example/task_finalization/service/AuthenticationService.java
+++ src/main/java/com/example/task_finalization/service/AuthenticationService.java
@@ -1,12 +1,19 @@
 package com.example.task_finalization.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final JwtDecoder jwtDecoder;
+
 
     public String getTokenFromContext() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -14,6 +21,11 @@ public class AuthenticationService {
             return ((JwtAuthenticationToken) auth).getToken().getTokenValue();
         }
         return null;
+    }
+
+    public void verifyToken(String bearerToken) {
+        String token = bearerToken.replace("Bearer ", "");
+        Jwt jwt = jwtDecoder.decode(token);
     }
 
 }
