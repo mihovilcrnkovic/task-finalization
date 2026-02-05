ccd08980f6c8a82e453a0ddd11d1fa2da6585a14
mihovilcrnkovic <mihovil.crnkovic@sigit.hr>
2026-02-05 16:32:14 +0100

    setContextAuthentication on kafka listener

--- src/main/java/com/example/task_finalization/service/KafkaConsumer.java
+++ src/main/java/com/example/task_finalization/service/KafkaConsumer.java
@@ -4,6 +4,8 @@ import com.example.task_finalization.model.ProcessingJob;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import tools.jackson.databind.json.JsonMapper;
 
@@ -14,12 +16,24 @@ public class KafkaConsumer {
 
     final JsonMapper jsonMapper;
     final FinalizationService finalizationService;
+    final AuthenticationService authenticationService;
 
     @KafkaListener(topics = "my-topic")
-    public void consumeMessage(ProcessingJob message){
+    public void consumeMessage(ProcessingJob message, @Header("Authorization") String authorization){
         String stringMsg = jsonMapper.writeValueAsString(message);
-        log.info("CONSUMED MESSAGE: " + stringMsg);
+        try {
+            authenticationService.setContextAuthentication(authorization);
+            log.info("CONSUMED MESSAGE: " + stringMsg);
 
-        finalizationService.finalizeJob(message);
+            finalizationService.finalizeJob(message);
+        } catch (Exception e) {
+            String errorMsg = "Exception trying to receive message: " +
+                    stringMsg + "\n\n" +
+                    e.getMessage();
+            log.error(errorMsg);
+        } finally {
+            SecurityContextHolder.clearContext();
+        }
+
     }
 }
