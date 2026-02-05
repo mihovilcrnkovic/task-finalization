b326e32b1dc1c16ceba777fc04550f0bd47531d0
mihovilcrnkovic <mihovil.crnkovic@sigit.hr>
2026-02-05 15:40:52 +0100

    authorization on RabbitMessageSender

--- src/main/java/com/example/task_finalization/service/RabbitMessageSender.java
+++ src/main/java/com/example/task_finalization/service/RabbitMessageSender.java
@@ -2,6 +2,7 @@ package com.example.task_finalization.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +13,15 @@ public class RabbitMessageSender {
 
     final RabbitTemplate rabbitTemplate;
     final FanoutExchange processingJobExchange;
+    final AuthenticationService authenticationService;
 
     public void sendMessage(Object message) {
-        rabbitTemplate.convertAndSend(processingJobExchange.getName(), "", message);
+        rabbitTemplate.convertAndSend(processingJobExchange.getName(), "", message, this::setMessageProperties);
+    }
+
+    private Message setMessageProperties(Message msg) {
+        String token = authenticationService.getTokenFromContext();
+        msg.getMessageProperties().setHeader("Authorization", "Bearer " + token);
+        return msg;
     }
 }
