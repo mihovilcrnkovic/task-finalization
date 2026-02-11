db2f18daa1e286bbb95aaec3abc4cc5c60b1fe6a
mihovilcrnkovic <mihovil.crnkovic@sigit.hr>
2026-02-11 13:22:12 +0100

    configured keycloak for integration tests

--- src/test/java/com/example/task_finalization/AbstractIntegrationTest.java
+++ src/test/java/com/example/task_finalization/AbstractIntegrationTest.java
@@ -1,5 +1,11 @@
 package com.example.task_finalization;
 
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import org.junit.jupiter.api.BeforeAll;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.AccessTokenResponse;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -10,19 +16,52 @@ import org.testcontainers.utility.DockerImageName;
 
 public abstract class AbstractIntegrationTest {
 
+    private static String kcAdminUsername = "admin";
+    private static String kcAdminPassword = "admin";
+    private static String kcClientId = "test-client";
+    private static String realmName = "test-realm";
+    private static String kcTestUsername = "test";
+    private static String kcTestPassword = "test";
+    public static String token;
+
     @ServiceConnection
     public static PostgreSQLContainer postgres = new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
     public static ConfluentKafkaContainer kafka = new ConfluentKafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0"));
     public static RabbitMQContainer rabbit = new RabbitMQContainer(DockerImageName.parse("rabbitmq:3-management"));
+    public static KeycloakContainer keycloak = new KeycloakContainer()
+            .withAdminUsername(kcAdminUsername)
+            .withAdminPassword(kcAdminPassword)
+            .withRealmImportFile("keycloak/test-realm.json");
 
     static {
         postgres.start();
         kafka.start();
         rabbit.start();
+        keycloak.start();
+    }
+
+    @BeforeAll
+    public static void createKeycloakToken() {
+        String authServerUrl = keycloak.getAuthServerUrl();
+        Keycloak kc = KeycloakBuilder.builder()
+                .serverUrl(authServerUrl)
+                .clientId(kcClientId)
+                .realm(realmName)
+                .username(kcTestUsername)
+                .password(kcTestPassword)
+                .grantType(OAuth2Constants.PASSWORD)
+                .build();
+
+        AccessTokenResponse tokenResponse = kc.tokenManager().getAccessToken();
+        token = tokenResponse.getToken();
     }
 
     @DynamicPropertySource
     public static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri", () -> keycloak.getAuthServerUrl() + "/realms/test-realm");
+        registry.add("spring.security.oauth2.resourceserver.jwt.jwk-set-uri", () -> keycloak.getAuthServerUrl() + "/realms/test-realm/protocol/openid-connect/certs");
+        registry.add("jwt.auth.converter.resource-id", () -> "oauth-client");
+
         registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
         registry.add("spring.kafka.producer.key-serializer", () -> "org.apache.kafka.common.serialization.StringSerializer");
         registry.add("spring.kafka.producer.value-serializer", () -> "org.springframework.kafka.support.serializer.JacksonJsonSerializer");
