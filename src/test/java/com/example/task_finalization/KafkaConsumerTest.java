c55f28c893002a035471e64968ea82c579fbb545
mihovilcrnkovic <mihovil.crnkovic@sigit.hr>
2026-02-11 14:01:45 +0100

    await database persistence (for kafka listener to finish)

--- src/test/java/com/example/task_finalization/KafkaConsumerTest.java
+++ src/test/java/com/example/task_finalization/KafkaConsumerTest.java
@@ -4,10 +4,7 @@ import com.example.task_finalization.model.Finalization;
 import com.example.task_finalization.model.ProcessingJob;
 import com.example.task_finalization.repository.FinalizationRepository;
 import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.common.protocol.types.Field;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,17 +12,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.kafka.support.KafkaUtils;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
+import org.testcontainers.shaded.org.awaitility.Awaitility;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
-import java.util.regex.Pattern;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -64,10 +62,17 @@ public class KafkaConsumerTest extends AbstractIntegrationTest {
                 .build();
         kafkaTemplate.send(msg).get();
 
-        Optional<Finalization> optionalFinalization = finalizationRepository.findFinalizationByTaskId(processingJob.getTaskId());
+
         ConsumerRecords<String, Object> records = KafkaTestUtils.getRecords(testConsumer);
 
         assertTrue(!records.isEmpty());
-        assertTrue(optionalFinalization.isPresent());
+
+        Awaitility.await()
+                .atMost(Duration.of(5, ChronoUnit.SECONDS))
+                .untilAsserted(() -> {
+                    Optional<Finalization> optionalFinalization = finalizationRepository.findFinalizationByTaskId(processingJob.getTaskId());
+                    assertTrue(optionalFinalization.isPresent());
+                });
+        ;
     }
 }
