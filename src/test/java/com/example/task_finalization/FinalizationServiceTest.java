16824cc44d730ef3771a1d5e6ffa3e58c2a07a22
mihovilcrnkovic <mihovil.crnkovic@sigit.hr>
2026-02-17 17:03:28 +0100

    added unit test for FinalizationService

---
+++ src/test/java/com/example/task_finalization/FinalizationServiceTest.java
@@ -0,0 +1,54 @@
+package com.example.task_finalization;
+
+import com.example.task_finalization.model.Finalization;
+import com.example.task_finalization.model.ProcessingJob;
+import com.example.task_finalization.repository.FinalizationRepository;
+import com.example.task_finalization.service.FinalizationService;
+import com.example.task_finalization.service.RabbitMessageSender;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class FinalizationServiceTest {
+
+    @Mock
+    RabbitMessageSender rabbitMessageSender;
+
+    @Mock
+    FinalizationRepository finalizationRepository;
+
+    @InjectMocks
+    FinalizationService finalizationService;
+
+    @Test
+    void should_CreateFinalization_when_JobFinalized() {
+        ProcessingJob job = ProcessingJob.builder()
+                .id(UUID.randomUUID())
+                .result("Test")
+                .taskId(UUID.randomUUID())
+                .processedAt(LocalDateTime.now())
+                .receivedAt(LocalDateTime.now())
+                .build();
+
+        Finalization finalization = Finalization.builder()
+                .taskId(job.getTaskId())
+                .finalizedAt(LocalDateTime.now())
+                .outcome("Success!")
+                .build();
+
+        when(finalizationRepository.save(any(Finalization.class))).thenReturn(finalization);
+
+        Finalization result = finalizationService.finalizeJob(job);
+        assertTrue(result.equals(finalization));
+    }
+}
