b7d4c1a5f5cad8d2625d3a4c8eb546d474337931
mihovilcrnkovic <mihovil.crnkovic@sigit.hr>
2026-02-25 16:44:54 +0100

    configured rabbit admin

--- src/main/java/com/example/task_finalization/configuration/RabbitConfig.java
+++ src/main/java/com/example/task_finalization/configuration/RabbitConfig.java
@@ -4,6 +4,8 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -37,5 +39,10 @@ public class RabbitConfig {
     @Bean
     public JacksonJsonMessageConverter jsonMessageConverter() {
         return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
     }
 }
